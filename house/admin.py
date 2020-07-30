872ae629eed52b06470246d18bf8c0ea3c6253c5
”OnurBoynuegri” <onurboynueri@gmail.com>
2020-07-30 13:01:55 +0300

    House tablosu olusturma ve Category tablosu ile iliskilendirme

--- house/admin.py
+++ house/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from house.models import Category
+from house.models import Category, House
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
+    list_filter = ['status']
+
+
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'room']
+    list_filter = ['status']
 
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(House, HouseAdmin)
--- house/models.py
+++ house/models.py
@@ -8,13 +8,61 @@ class Category(models.Model):
         ('False', 'Hayır'),
     )
     title = models.CharField(max_length=30)
-    keywords = models.CharField(max_length=255)
-    decription = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=255, blank=True)
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=30, choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True)
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class House(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    BALCONY = (
+        ('Existent', 'Var'),
+        ('Absent', 'Yok'),
+    )
+    HEATING = (
+        ('Yok', 'Yok'),
+        ('Soba', 'Soba'),
+        ('Kombi', 'Kombi'),
+        ('Merkezi', 'Merkezi'),
+    )
+    STUFF = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    ROOM = (
+        ('1+0', '1+0'),
+        ('1+1', '1+1'),
+        ('2+0', '2+0'),
+        ('2+1', '2+1'),
+        ('3+1', '3+1'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30, blank=True)
+    keywords = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    address = models.TextField()
+    price = models.IntegerField(blank=True)
+    room = models.CharField(max_length=30, choices=ROOM, blank=True)
+    balcony = models.CharField(max_length=30, choices=BALCONY, blank=True)
+    heating = models.CharField(max_length=30, choices=HEATING, blank=True)
+    stuff = models.CharField(max_length=30, choices=STUFF, blank=True)
+    status = models.CharField(max_length=30, choices=STATUS)
+    detail = models.TextField(blank=True)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
