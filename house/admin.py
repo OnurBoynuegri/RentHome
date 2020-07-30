dbccbde4af0c49f431587e883cf8045d7c9bccc6
”OnurBoynuegri” <onurboynueri@gmail.com>
2020-07-30 20:35:05 +0300

    Admin resim galerisi eklendi

--- house/admin.py
+++ house/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from house.models import Category, House
+from house.models import Category, House, Images
 
+class HouseImageInline(admin.TabularInline):
+    model = Images
+    extra = 3
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
@@ -12,7 +15,13 @@ class CategoryAdmin(admin.ModelAdmin):
 class HouseAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'price', 'room']
     list_filter = ['status']
+    inlines = [HouseImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'house', 'image']#buradaki title image ler home modeldeki ile aynı olmalı
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(House, HouseAdmin)
+admin.site.register(Images, ImagesAdmin)
