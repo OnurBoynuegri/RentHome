f825f95e238e8e10dc1fc1c008358a1321f4edb1
”OnurBoynuegri” <onurboynueri@gmail.com>
2020-07-31 19:46:52 +0300

    setting tablosu ve favicon eklendi

--- home/models.py
+++ home/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class Setting(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    company = models.CharField(max_length=50)
+    address = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50, blank=True)
+    fax = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=50, blank=True)
+    smtpserver = models.CharField(max_length=50, blank=True)
+    smtpemail = models.CharField(max_length=50, blank=True)
+    smtppassword = models.CharField(max_length=50, blank=True)
+    smtpport = models.CharField(max_length=50, blank=True)
+    icon = models.ImageField(blank=True, upload_to='images/')
+    facebook = models.CharField(max_length=50, blank=True)
+    instagram = models.CharField(max_length=50, blank=True)
+    twitter = models.CharField(max_length=50, blank=True)
+    aboutus = models.TextField(blank=True)
+    contact = models.TextField(blank=True)
+    references = models.TextField(blank=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
