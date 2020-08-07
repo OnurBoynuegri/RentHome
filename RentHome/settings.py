165ae11b1363fccb172baa0f60fa24b26b06870b
Onur Boynueğri <onurboynuegri@hotmail.com>
2020-08-07 21:18:02 +0300

    Anasayfa Uye Paneli ve Profil Bilgileri Eklendi

--- RentHome/settings.py
+++ RentHome/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
     'house.apps.HouseConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
