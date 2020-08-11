508c184e0229cdaec80c6f12e37a62f0b416a728
Onur Boynueğri <onurboynuegri@hotmail.com>
2020-08-11 15:49:21 +0300

    User ilan ekleme ve user ilanlarının user profilinde listelenmesi

--- user/models.py
+++ user/models.py
@@ -1,3 +1,31 @@
+from ckeditor.widgets import CKEditorWidget
 from django.db import models
 
 # Create your models here.
+from django.forms import ModelForm, TextInput, FileInput, Select, NumberInput
+
+from house.models import House
+
+
+class UserRentHouseForm(ModelForm):
+    class Meta:
+        model = House
+        fields = ('category','title', 'keywords','description','image','address','price','room','balcony',
+                  'heating','stuff','detail','slug',)
+        widgets = {
+            'category': Select(attrs={'class': 'form-control', 'placeholder': 'Category'}),
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'keywords': TextInput(attrs={'class': 'form-control', 'placeholder': 'Keywords'}),
+            'description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Description'}),
+            'image': FileInput(attrs={'class': 'form-control', 'placeholder': 'image'}),
+
+            'address': TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}),
+            'price': NumberInput(attrs={'class': 'form-control', 'placeholder': 'Price'}),
+            'room': Select(attrs={'class': 'form-control', 'placeholder': 'Room'}),
+            'balcony': Select(attrs={'class': 'form-control', 'placeholder': 'balcony'}),
+            'heating': Select(attrs={'class': 'form-control', 'placeholder': 'heating'}),
+            'stuff': Select(attrs={'class': 'form-control', 'placeholder': 'stuff'}),
+            'detail': CKEditorWidget(),
+            'slug': TextInput(attrs={'class': 'form-control', 'placeholder': 'Slug'}),
+
+        }
--- user/views.py
+++ user/views.py
@@ -1,13 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from home.models import UserProfile
-from house.models import Category
+from house.models import Category, House
 from user.forms import UserUpdateForm, ProfileUpdateForm
+from user.models import UserRentHouseForm
 
 
 def index(request):
@@ -17,6 +19,7 @@ def index(request):
     context = {'category': category,
                'profile': profile}
     return render(request, 'user_profile.html', context)
+
 
 def user_update(request):
     if request.method == 'POST':
@@ -64,3 +67,57 @@ def change_password(request):
             'category': category,
             'profile': profile,
         })
+
+
+@login_required(login_url='/login')
+def user_rent(request):
+    category = Category.objects.all()
+    current_user = request.user
+    profile = UserProfile.objects.get(user_id=current_user.id)
+    house = House.objects.filter(user_id=current_user.id)
+    form = UserRentHouseForm()
+    context = {
+        'category': category,
+        'house': house,
+        'profile': profile,
+        'form': form
+    }
+    return render(request, 'user_rent.html', context)
+
+
+@login_required(login_url='/login')
+def add_rent(request):
+    if request.method == 'POST':
+        form = UserRentHouseForm(request.POST, request.FILES)
+        if form.is_valid():
+            current_user = request.user
+            data = House()
+            data.user_id = current_user.id
+            data.category = form.cleaned_data['category']
+            data.title = form.cleaned_data['title']
+            data.keywords = form.cleaned_data['keywords']
+            data.description = form.cleaned_data['description']
+            data.image = form.cleaned_data['image']
+            data.address = form.cleaned_data['address']
+            data.price = form.cleaned_data['price']
+            data.room = form.cleaned_data['room']
+            data.balcony = form.cleaned_data['balcony']
+            data.heating = form.cleaned_data['heating']
+            data.stuff = form.cleaned_data['stuff']
+            data.status = 'False'
+            data.detail = form.cleaned_data['detail']
+            data.slug = form.cleaned_data['slug']
+            data.save()
+            messages.success(request, "Başarıyla eklendi")
+            return HttpResponseRedirect('/user/userrent')
+        else:
+            messages.success(request, " Hata")
+            return HttpResponseRedirect('/')
+    else:
+        category = Category.objects.all()
+        form = UserRentHouseForm()
+        context = {
+            'category': category,
+            'form': form
+        }
+        return render(request, 'user_addrent.html', context)
